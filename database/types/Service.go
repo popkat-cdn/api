d2760b6434c4d3faa0c505345cfe782ee98acb34
selectdev <selectdev01@gmail.com>
2023-12-06 05:42:43 +0000

    improved db typings

--- database/types/Service.go
+++ database/types/Service.go
@@ -1,8 +1,8 @@
 package types
 
 type Service struct {
-	Name  string
-	ID    string
-	Icon  string
-	Token string
+	Name  string `db:"name" json:"name" validate:"required" description:"Service Name"`
+	ID    string `db:"id" json:"id" validate:"required" description:"Service ID"`
+	Icon  string `db:"icon" json:"icon" validate:"required" description:"Service Icon"`
+	Token string `db:"token" json:"token" validate:"required" description:"Service API Token"`
 }
--- database/types/User.go
+++ database/types/User.go
@@ -1,9 +1,9 @@
 package types
 
 type User struct {
-	Username string
-	UserID   string
-	Banned   bool
-	Avatar   string
-	Token    string
+	Username string `db:"username" json:"username" validate:"required" description:"Username"`
+	UserID   string `db:"userid" json:"user_id" validate:"required" description:"User ID"`
+	Banned   bool   `db:"banned" json:"banned" validate:"required" description:"Is the user banned?"`
+	Avatar   string `db:"avatar" json:"avatar" validate:"required" description:"User Avatar"`
+	Token    string `db:"token" json:"token" validate:"required" description:"User Auth Token (temp)"`
 }
